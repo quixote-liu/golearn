4a40e8c961748c92ea63f1ff38de089a37ac9fa7
liuchengshun <lcs.shun@foxmail.com>
2022-05-04 11:06:20 +0800

    prototype

--- design_mode/prototype/prototype.go
+++ design_mode/prototype/prototype.go
@@ -19,6 +19,23 @@ func (k *Keyword) Clone() *Keyword {
 	return &nk
 }
 
+// Keywords 关键字 map
 type Keywords map[string]*Keyword
 
-func (k *Keywords) Clone()
+// Clone 复制一个新的keywords
+// updateWords: 需要更新的关键词列表，由于从数据库中获取常常是数组的方式
+func (words Keywords) Clone(updateWords []*Keyword) Keywords {
+	newKeywords := Keywords{}
+
+	// 这里是浅拷贝，直接拷贝了地址
+	for k, v := range words {
+		newKeywords[k] = v
+	}
+
+	// 替换掉需要更新的字段，这里用的是深拷贝
+	for _, word := range updateWords {
+		newKeywords[word.Word] = word.Clone()
+	}
+
+	return newKeywords
+}
