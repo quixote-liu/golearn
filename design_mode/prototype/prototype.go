24372ee2e152a50510a1bf8b3cc17ac09ef8aea2
liuchengshun <lcs.shun@foxmail.com>
2022-05-04 10:36:31 +0800

    update readme

--- design_mode/prototype/prototype.go
+++ design_mode/prototype/prototype.go
@@ -1,2 +1,24 @@
 package prototype
 
+import (
+	"encoding/json"
+	"time"
+)
+
+type Keyword struct {
+	Word     string
+	Visit    int
+	UpdateAt *time.Time
+}
+
+// 通过序列化和反序列化深拷贝
+func (k *Keyword) Clone() *Keyword {
+	var nk Keyword
+	b, _ := json.Marshal(k)
+	_ = json.Unmarshal(b, &nk)
+	return &nk
+}
+
+type Keywords map[string]*Keyword
+
+func (k *Keywords) Clone()
